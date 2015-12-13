ff28b8cfd5cb8194c68dfe9e7cb996ba1531ed3a
vkeygarg <v.garg2@gmail.com>
2015-12-12 19:48:24 -0800

    Additional file for getting item details with Index file creation

--- ExtractAgileData/src/main/java/com/x/agile/batchjob/extract/action/ExtractDataAction.java
+++ ExtractAgileData/src/main/java/com/x/agile/batchjob/extract/action/ExtractDataAction.java
@@ -2,12 +2,9 @@ package com.x.agile.batchjob.extract.action;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.log4j.Logger;
 
